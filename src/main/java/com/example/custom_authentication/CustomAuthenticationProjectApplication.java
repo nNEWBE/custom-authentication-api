7cdcd92f7f2ff98aac1cb0e93b0bd9bb524d9c45
Shuvo Chandra Deb Nath <shuvochandra999@gmail.com>
2026-01-30 09:29:09 +0600

    Project Initialized

---
+++ src/main/java/com/example/custom_authentication/CustomAuthenticationProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.custom_authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomAuthenticationProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomAuthenticationProjectApplication.class, args);
+	}
+
+}
